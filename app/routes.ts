b265a80af880699e6b76983032513d8424e350d8
Alwaz <alwazkazi3@gmail.com>
2025-07-26 17:15:41 +0500

    chore: initial commit

---
+++ app/routes.ts
@@ -0,0 +1,8 @@
+/*
+ This is router config file, contains all the routes
+
+*/
+
+import { type RouteConfig, index } from "@react-router/dev/routes";
+
+export default [index("routes/home.tsx")] satisfies RouteConfig;
