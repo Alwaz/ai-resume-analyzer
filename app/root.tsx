7ab0d0b06c23dc0a4c6f1090c155fb745a573500
Alwaz <alwazkazi3@gmail.com>
2025-07-27 16:56:09 +0500

    feat(auth): Auth flow implemented using puter

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { usePuterStore } from "./lib/puter";
+import { useEffect } from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -29,6 +31,11 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]);
   return (
     <html lang="en">
       <head>
@@ -38,6 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
